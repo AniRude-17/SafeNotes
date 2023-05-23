dab2dd9ab83e6455910412bc3783c4276faab255
anir-ab <anibhagwat17@gmail.com>
2023-05-23 20:10:10 +0530

    make note implemented, just the working

--- frontend/notes-frontend/src/assets/pages/View.jsx
+++ frontend/notes-frontend/src/assets/pages/View.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AskPass from '../popups/AskPass';
+import MakeNote from '../popups/MakeNote';
+import axios from 'axios';
+
+
 
 const View = () => {
   const id=useParams().id;
@@ -14,7 +18,8 @@ const View = () => {
   return (
     <div>
       <h3>VIEW NOTE</h3>
-      <AskPass Id={id} setNoteContent={setNoteContent} />
+      {/* <AskPass Id={id} setNoteContent={setNoteContent} /> */}
+      <MakeNote Id={id} setNoteContent={setNoteContent}/>
       <input type="text" value={noteContent} id="noteText" onChange={handleNoteEdit} />
     </div>
   );
---
+++ frontend/notes-frontend/src/assets/popups/MakeNote.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const MakeNote = ({Id,setNoteContent}) => {
+
+    const [show, setShow] = useState(true);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    const [password, setPassword] = useState('');
+    const handleChange = (event) => {
+        setPassword(event.target.value);
+        console.log(event.target.value)
+      };
+
+    const handlePass = () =>{
+        console.log(Id);
+        const postData = {
+            id: Id,
+            passhash: password,
+            content: " "
+          };
+          axios.post('http://localhost:8000/add', postData)
+          .then(response => {
+            setShow(false);
+            setNoteContent("The Note is Currently Empty :(");
+            // Handle the response as needed
+          })
+          .catch(error => {
+            setShow(false);
+            console.error(error);
+            // Handle the error as needed
+          });
+    }
+
+  return (
+    <div>MakeNote
+        <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Note Does Not Exists
+            To create on this name enter the password
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" id="passInput">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="pupu Password"
+               value={password}
+               onChange={handleChange}
+              autoFocus
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={handlePass}>
+          Submit
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    </div>
+  )
+}
+
+export default MakeNote
